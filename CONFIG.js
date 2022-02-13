75626e648d0f3ba3d0f6e98c0800ef883db83a27
Cyberlovee <88539892+Cyberlovee@users.noreply.github.com>
2022-02-13 19:11:57 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Toi tặng quà valen thì c có nhận hong",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu b thoát ra và không trả lời thì có nghĩa là b đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Okela...",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Vì lý do địa lý nên hong gửi đc ^^ </br> Cho nợ đi nào gặp toi trả sau :)) </br> "Từ mai b học r nên khó rủ game đc. </br>  Chúc b học tốt "',
 };
